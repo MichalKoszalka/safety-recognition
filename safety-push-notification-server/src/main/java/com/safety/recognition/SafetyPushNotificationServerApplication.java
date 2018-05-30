9c9b1debe46dbed86a3cebb71c32a1d2e7b918a2
Michał Koszałka <michal.koszalka@jitsolutions.pl>
2018-05-30 12:40:54 +0200

    initial commit

---
+++ safety-push-notification-server/src/main/java/com/safety/recognition/SafetyPushNotificationServerApplication.java
@@ -0,0 +1,12 @@
+package com.safety.recognition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafetyPushNotificationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafetyPushNotificationServerApplication.class, args);
+	}
+}
---
+++ spark-processor/src/main/scala/SimpleApp.scala
@@ -0,0 +1,19 @@
+import java.util.Scanner
+
+import org.apache.spark.sql.SparkSession
+
+object SimpleApp {
+
+  def main(args: Array[String]): Unit = {
+    val logFile = "spark-processor/src/main/resources/sampleFile"
+    val spark = SparkSession.builder().appName("Simple Application").master("local").getOrCreate()
+    val logData = spark.read.textFile(logFile).cache()
+    val numsC = logData.filter(line => line.contains("c")).count()
+    val numsB = logData.filter(line => line.contains("b")).count()
+    val scanner = new Scanner(System.in)
+    scanner.nextLine()
+    println(s"Lines with c: $numsC, Lines with b: $numsB")
+    spark.stop()
+  }
+
+}
